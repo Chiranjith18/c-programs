3cec844def5c7e21dffc6092acad441f53b97866
Chiranjith18 <145703565+Chiranjith18@users.noreply.github.com>
2024-09-24 00:24:22 +0530

    Create print 1 to n usingf backtracking.cpp

---
+++ print 1 to n usingf backtracking.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace  std;
+int count;
+void print(int i,int n) {
+    if(i==0) {
+        return;
+    }
+   print(i-1,n);
+    cout<<i<<endl;
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n,n);
+    return 0;
+}

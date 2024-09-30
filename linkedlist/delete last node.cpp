af048e7d9b038996ebec62f5db98511d8b36ba58
Aarna chauhan <chauhanaarna6@gmail.com>
2024-09-30 16:17:47 +0400

    Create delete last node.cpp

---
+++ linkedlist/delete last node.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* next;
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+void printlist(Node* head){
+    Node* curr=head;
+    for(curr=head;curr!=nullptr;curr=curr->next){
+        cout<<curr->data<<"->";
+    }
+    cout<<"Null";
+}
+Node deletelast(Node* thread){
+    if(thread==nullptr){
+        return nullptr;
+    }
+    if(thread->next==nullptr){
+        delete thread;
+        return nullptr;
+    }
+    Node *curr=thread;
+    while(curr->next->next!=nullptr){
+        curr=next->next;
+    }
+    delete(curr->next);
+    curr->next=nullptr;
+    return thread;
+}
+
+
+int main() {
+    Node* thread=new Node(9);
+    thread->next=new Node(10);
+    thread->next->next=new Node(11);
+    printlist(thread);
+    int newlist=deletelast(thread);
+    printlist(newlist);
+
+    return 0;
+}

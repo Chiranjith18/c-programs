a5632432043d75596cefce4a97e2d0ba14c63b73
Aarna chauhan <chauhanaarna6@gmail.com>
2024-09-30 20:15:51 +0400

    Create insert at end.cpp

---
+++ linkedlist/insert at end.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+struct Node{
+   int data;
+   Node* next;
+   Node(int data1){
+       data=data1;
+       next=nullptr;
+   }
+};
+void printlist(Node* head){
+    Node* curr=head;
+    for(curr=head;curr!=nullptr;curr=curr->next){
+        cout<<curr->data<<"->";
+    }
+    cout<<"Null"<<endl;
+}
+//new head
+
+Node* insert(Node* head,int data){
+    Node* newnode=new Node(data);
+    newnode->next=head;
+    head=newnode;
+    return head;
+}
+Node* insertend(Node* head,int data){
+    while(head->next->next!=nullptr){
+       head=head->next;
+    }
+    Node* newnode=new Node(data);
+    head->next=newnode;
+   return head;
+    
+}
+
+
+
+int main() {
+    Node* head=nullptr;
+    head=insert(head,5);
+    head=insert(head,10);
+    head=insert(head,15);
+    head=insert(head,20);
+    cout<<"linked list before";
+    printlist(head);
+    head=insertend(head,3);
+    cout<<"linked list after";
+    printlist(head);
+
+    return 0;
+}

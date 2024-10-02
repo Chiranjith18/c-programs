6db1979e970fc86488b0aeae4dc424d9e53685d3
Aarna chauhan <chauhanaarna6@gmail.com>
2024-10-02 21:10:35 +0400

    Update left rotate by 1.cpp

--- arrays/left rotate by 1.cpp
+++ arrays/left rotate by 1.cpp
@@ -2,21 +2,26 @@
 #include<vector>
 #include<iostream>
 using namespace std;
-int rotate(int arr[],int n,int temp){
-    temp=arr[0];
+void rotate(int arr[],int n){
+   int temp=arr[0];
    for(int i=1;i<n;i++){
        arr[i-1]=arr[i];
    }
-   arr[n-1]=temp[0];
+   arr[n-1]=temp;
+}
+void print(int arr[],int n){
+   for(int i=0;i<n;i++){
+      cout<<arr[i]<<" "; 
+   }
 }
 
 int main(){
     int arr[]={1,2,3,4,5};
-    int n=arr.size();
-    int temp;
-    temp=arr[0];
-    arr=rotate(arr,n,temp);
-    cout<<"rotated by 1";
+    int n=sizeof(arr)/sizeof(arr[0]);
+    rotate(arr,n);
+    cout<<"rotated by 1"<<endl;
+    print(arr,n);
+   
     
     return 0;
 }

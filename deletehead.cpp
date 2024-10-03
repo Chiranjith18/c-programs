7b48312cd0bf45545e332ebefad1e41b813ddc0a
Chiranjith18 <145703565+Chiranjith18@users.noreply.github.com>
2024-10-03 17:41:35 +0530

    Create deletehead.cpp

---
+++ deletehead.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node {
+    int data;
+    Node*next;
+    Node(int data1) {
+        data=data1;
+        next=nullptr;
+    }
+};
+int main() {
+    Node* head = new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+Node* curr = head;
+    head=head->next;
+    delete(curr);
+    while(head!=nullptr) {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+}
